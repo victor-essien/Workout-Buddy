321cac428e65ebcf38272da562b720071665a8b8
Victor Essien <viktoressien1@gmail.com>
2024-01-05 22:17:25 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+const cors = require('cors')
+// express app
+const app = express()
+
+// Enable CORS for all routes
+app.use(cors());
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+
+
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
